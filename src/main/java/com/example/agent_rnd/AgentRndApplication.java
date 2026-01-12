e834f914df7c7b09b344c8450bba4183c95becee
juk <jeonguk5133@gmail.com>
2026-01-12 13:20:53 +0900

    New agent

---
+++ src/main/java/com/example/agent_rnd/AgentRndApplication.java
@@ -0,0 +1,13 @@
+package com.example.agent_rnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentRndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentRndApplication.class, args);
+	}
+
+}
